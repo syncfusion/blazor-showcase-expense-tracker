cc82846560f924498b5f6330c83f49fcb45d6804
Sridhar Alagiri Samy <sridhar.alagirisamy@syncfusion.com>
2019-11-22 19:08:11 +0530

    Moved Expense Tracker application to github

---
+++ server-side/ExpenseTracker/Models/CategoryData.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseTracker.Models
+{
+    public class CategoryData
+    {
+        public string Class { get; set; }
+        public string Category { get; set; }
+        public string Id { get; set; }
+    }
+}
---
+++ server-side/ExpenseTracker/Services/AppState.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpenseTracker.Pages.Expense;
+
+namespace ExpenseTracker.Service
+{
+    public class AppState
+    {
+        public ExpenseDataService ExpenseDataService { get; set; }
+
+        public string CurrentBalance { get; set; }
+        public FilterMenu FilterComponent { get; private set; }
+
+        public event Action OnChange;
+
+        public AppState()
+        {
+            ExpenseDataService = new ExpenseDataService();
+        }
+
+        public void SetCommonData(ExpenseDataService data)
+        {
+            this.ExpenseDataService = data;
+            NotifyStateChanged();
+        }
+
+        public void UpdateCurrentBalance(string balance)
+        {
+            this.CurrentBalance = balance;
+            NotifyStateChanged();
+        }
+
+        public void SetFilterComponent(FilterMenu component)
+        {
+            this.FilterComponent = component;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
