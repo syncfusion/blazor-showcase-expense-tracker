14e56fd3b93b4178d1a3aa7d897b0acbf6bfa732
Sridhar Alagiri Samy <sridhar.alagirisamy@syncfusion.com>
2020-07-14 16:51:03 +0530

    Expense Tracker App upgraded to volume 2 2020 release and folder names changed

---
+++ ExpenseTracker.Client/Services/CommonService.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace ExpenseTracker.Service
+{
+    public static class CommonService
+    {
+        public static string GetCurrencyVal(int val)
+        {
+            CultureInfo NewCulture = new CultureInfo("en-US");
+            if (val < 0)
+            {
+                NewCulture.NumberFormat.CurrencyNegativePattern = 1;
+            }
+            string formattedValue = val.ToString("C0", NewCulture);
+            return formattedValue;
+        }
+
+        public static string GetNumberVal(int val)
+        {
+            return val.ToString("0,0", new CultureInfo("en-US"));
+        }
+    }
+}
---
+++ ExpenseTracker.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExpenseTracker.Shared
+{
+    public class Class1
+    {
+    }
+}
